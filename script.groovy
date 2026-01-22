0c5c8b7e45dd8120b3abf51045c1248675ee355c
Arun V <vuyala.arun@gmail.com>
2026-01-22 16:47:13 -0600

    updated jenkins

--- script.groovy
+++ script.groovy
@@ -7,9 +7,9 @@ def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
                   sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
-                  sh 'docker build -t myapp:latest .'
-                  sh 'docker tag myapp:latest vuyalaarun/myapp:latest'
-                  sh 'docker push vuyalaarun/myapp:latest'
+                  sh 'docker build -t myapp:2.0 .'
+                  sh 'docker tag myapp:2.0 vuyalaarun/myapp:2.0'
+                  sh 'docker push vuyalaarun/myapp:2.0'
     }
 }
 
